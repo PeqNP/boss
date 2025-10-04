6834522784a8d1887a577d62d348b0bd34f24885
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2025-10-04 07:48:50 -0700

    Add new friend Fix: All scripts now use correct attribute, type Fix: Add friend is a modal New: If a modal is loaded as a window, the UI library will now tell you and offer a suggestion. New: AddFriend controller is complete New: Loading friends is now complete Redesigning the friends UI again New: email HTML input field is now supported

--- server/web/Sources/App/Routes/Friend/FriendRoute.swift
+++ server/web/Sources/App/Routes/Friend/FriendRoute.swift
@@ -9,8 +9,7 @@ public func registerFriend(_ app: Application) {
     app.group("friend") { group in
         group.get { req in
             let authUser = try await verifyAccess(cookie: req)
-            let fragment = Fragment.Friends(friendRequests: [], yourRequests: [], friends: [])
-            return fragment
+            return try await makeFriends(for: authUser)
         }.openAPI(
             summary: "Get all friends, friend requests, and pending requests",
             response: .type(Fragment.Friends.self),
@@ -19,8 +18,9 @@ public func registerFriend(_ app: Application) {
         
         group.post("add") { req in
             let authUser = try await verifyAccess(cookie: req)
-            let fragment = Fragment.Friends(friendRequests: [], yourRequests: [], friends: [])
-            return fragment
+            let form = try req.content.decode(FriendForm.AddFriend.self)
+            try await api.friend.addFriend(user: authUser.user, email: form.email)
+            return try await makeFriends(for: authUser)
         }.openAPI(
             summary: "Add a new friend",
             body: .type(FriendForm.AddFriend.self),
@@ -31,8 +31,9 @@ public func registerFriend(_ app: Application) {
         
         group.post("remove") { req in
             let authUser = try await verifyAccess(cookie: req)
-            let fragment = Fragment.Friends(friendRequests: [], yourRequests: [], friends: [])
-            return fragment
+            let form = try req.content.decode(FriendForm.RemoveFriend.self)
+            try await api.friend.removeFriend(user: authUser.user, id: form.id)
+            return try await makeFriends(for: authUser)
         }.openAPI(
             summary: "Remove a friend",
             body: .type(FriendForm.RemoveFriend.self),
@@ -43,8 +44,9 @@ public func registerFriend(_ app: Application) {
         
         group.post("accept") { req in
             let authUser = try await verifyAccess(cookie: req)
-            let fragment = Fragment.Friends(friendRequests: [], yourRequests: [], friends: [])
-            return fragment
+            let form = try req.content.decode(FriendForm.AcceptFriendRequest.self)
+            try await api.friend.acceptFriendRequest(user: authUser.user, id: form.id)
+            return try await makeFriends(for: authUser)
         }.openAPI(
             summary: "Accept a friend request",
             body: .type(FriendForm.AcceptFriendRequest.self),
@@ -55,8 +57,9 @@ public func registerFriend(_ app: Application) {
         
         group.post("reject") { req in
             let authUser = try await verifyAccess(cookie: req)
-            let fragment = Fragment.Friends(friendRequests: [], yourRequests: [], friends: [])
-            return fragment
+            let form = try req.content.decode(FriendForm.RejectFriendRequest.self)
+            try await api.friend.removeFriendRequest(user: authUser.user, id: form.id)
+            return try await makeFriends(for: authUser)
         }.openAPI(
             summary: "Reject friend request",
             body: .type(FriendForm.RejectFriendRequest.self),
@@ -65,4 +68,19 @@ public func registerFriend(_ app: Application) {
             responseContentType: .application(.json)
         )
     }
+}
+
+private func makeFriends(for authUser: AuthenticatedUser) async throws -> Fragment.Friends {
+    let friends = try await api.friend.friends(user: authUser.user)
+    let requests = try await api.friend.friendRequests(user: authUser.user)
+    return .init(
+        friendRequests: requests
+            .filter { $0.userId != authUser.user.id }
+            .map { $0.makeFriendRequestOption() },
+        yourRequests: requests
+            .filter { $0.userId == authUser.user.id }
+            .map { $0.makeMyFriendRequestOption() },
+        friends: friends
+            .map { $0.makeFriendOption() }
+    )
 }
