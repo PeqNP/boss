3dab31aa4e24f0f81682f60502a6496d76dfb22d
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2026-01-18 11:53:04 -0800

    Removed capacity planner from desktop Created VC that will demonstrate how to use and interact with all BOSS element types Fix: Hide error message in viewDidLoad (this is the standard BOSS practice)

--- private/app/io.bithead.boss/__init__.py
+++ private/app/io.bithead.boss/__init__.py
@@ -135,7 +135,6 @@ async def get_default(user_id: int, boss_user: User, request: Request):
         # TODO: Read user preferences
         value = {
             "desktop": [
-                AppLink(bundleId="io.bithead.capacity-planner", name="Capacity Planner", icon="icon.svg"),
                 AppLink(bundleId="io.bithead.test-manager", name="Test Manager", icon="icon.svg"),
                 AppLink(bundleId="io.bithead.json-formatter", name="JSON Formatter", icon="icon.svg"),
                 AppLink(bundleId="io.bithead.wordy", name="Wordy", icon="icon.svg")
