e740bb63dd18a6ddee7612085bc525aa83eb17ad
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2026-04-22 22:07:05 -0700

    - All object names are now making request to server to update the name.

--- server/web/Sources/App/Routes/Lean/Lean+Forms.swift
+++ server/web/Sources/App/Routes/Lean/Lean+Forms.swift
@@ -1,1 +1,25 @@
-/// Copyright ⓒ 2026 Bithead LLC. All rights reserved. 
+/// Copyright ⓒ 2026 Bithead LLC. All rights reserved.
+
+import Vapor
+
+enum LeanForm {
+    struct UpdateLineName: Content {
+        var id: Int
+        var name: String
+    }
+
+    struct UpdateStationName: Content {
+        var id: Int
+        var name: String
+    }
+
+    struct UpdateIntakeQueueName: Content {
+        var id: Int
+        var name: String
+    }
+
+    struct UpdateInventoryName: Content {
+        var id: Int
+        var name: String
+    }
+}
--- server/web/Sources/App/Routes/Lean/LeanRoute.swift
+++ server/web/Sources/App/Routes/Lean/LeanRoute.swift
@@ -7,5 +7,36 @@ import Vapor
 /// Register the `/lean/` routes.
 public func registerLean(_ app: Application) {
     app.group("lean") { group in
+        group.post("update-line-name") { req in
+            let _ = try req.authUser
+            let form = try req.content.decode(LeanForm.UpdateLineName.self)
+            // TODO: Update line name
+            _ = form
+            return Response(status: .ok)
+        }
+
+        group.post("update-station-name") { req in
+            let _ = try req.authUser
+            let form = try req.content.decode(LeanForm.UpdateStationName.self)
+            // TODO: Update station name
+            _ = form
+            return Response(status: .ok)
+        }
+
+        group.post("update-intake-queue-name") { req in
+            let _ = try req.authUser
+            let form = try req.content.decode(LeanForm.UpdateIntakeQueueName.self)
+            // TODO: Update intake queue name
+            _ = form
+            return Response(status: .ok)
+        }
+
+        group.post("update-inventory-name") { req in
+            let _ = try req.authUser
+            let form = try req.content.decode(LeanForm.UpdateInventoryName.self)
+            // TODO: Update inventory name
+            _ = form
+            return Response(status: .ok)
+        }
     }
 }
